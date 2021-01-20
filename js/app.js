04c837025649477c8f02609a366494d80ef005db
manideep519 <manideepyadav359@gmail.com>
2021-01-20 17:59:28 +0530

    v.2.0

--- js/app.js
+++ js/app.js
@@ -1,15 +1,28 @@
 AOS.init({
-    once: 'true'
+  once: "true",
+  offset: 100,
 });
 
-let menu = document.getElementById('menu');
-let nav = document.getElementById('nav');
-let list = document.getElementById('list');
+let menu = document.getElementById("menu");
+let nav = document.getElementById("nav");
+let list = document.getElementById("list");
+let navbar = document.getElementById("navbar");
 
+let sticky = nav.offsetTop;
 
-menu.addEventListener("click", function() {
-    list.classList.toggle('active')
-    nav.classList.toggle('active')
+menu.addEventListener("click", function () {
+  list.classList.toggle("active");
+  nav.classList.toggle("active");
 });
 
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
 
+window.onscroll = function () {
+  myFunction();
+};
