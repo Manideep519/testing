f4c6eab6b861f225c919181a1e22830948a4ae94
Manideep <35698382+Manideep519@users.noreply.github.com>
2021-01-31 10:44:54 +0530

    Add files via upload

---
+++ build/precache-manifest.22d963aeabd8bfde0ab64eb14df46436.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "178517893e08616496ed",
+    "url": "/static/css/main.f51c6a6e.chunk.css"
+  },
+  {
+    "revision": "178517893e08616496ed",
+    "url": "/static/js/main.17851789.chunk.js"
+  },
+  {
+    "revision": "b1cd6bc62d18039e4be9",
+    "url": "/static/css/1.6f7b2d5e.chunk.css"
+  },
+  {
+    "revision": "b1cd6bc62d18039e4be9",
+    "url": "/static/js/1.b1cd6bc6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "805a45810788b087332d7b1a4bab970d",
+    "url": "/index.html"
+  }
+];
